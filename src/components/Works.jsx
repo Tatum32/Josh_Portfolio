d165c7d5352bc3760cff609533a65d46e900b29e
Shitcoder731 <shitanshu.731@gmail.com>
2024-03-15 13:26:16 +0530

    unwanted items delete

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { motion } from "framer-motion";
-
 import { styles } from "../styles";
 import { arrow } from "../assets";
 import { SectionWrapper } from "../hoc";
 import { projects } from "../constants";
-import { fadeIn, textVariant } from "../utils/motion";
+import { fadeIn } from "../utils/motion";
 
 const ProjectCard = ({
   index,
