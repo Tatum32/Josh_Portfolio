3ea9c23d67153022e6f94594f5212110f4062b60
Shitcoder731 <shitanshu.731@gmail.com>
2024-03-15 12:54:34 +0530

    skills section fix

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,17 +1,18 @@
 import React from "react";
-
-import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 
 const Tech = () => {
   return (
-    <div className='flex flex-row flex-wrap justify-center gap-10'>
+    <div><h1 className="text-red-500  mb-20 w-full text-2xl font-bold  pl-[45%]">My Skills</h1>
+    <div className='flex flex-row flex-wrap justify-center gap-4'>
+      
       {technologies.map((technology) => (
-        <div className='w-28 h-28' key={technology.name}>
-          <BallCanvas icon={technology.icon} />
+        <div className='w-40 h-28' key={technology.name}>
+          <p className="text-center w-50 rounded-lg shadow-lg h-20 bg-white text-black font-semibold px-1 py-4">{technology.name}</p>
         </div>
       ))}
+    </div>
     </div>
   );
 };
